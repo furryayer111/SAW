5898aa2ee2281f9fa9f9234cbcdb4193cda3aba5
Tralalerotralala <103183973+furryayer111@users.noreply.github.com>
2026-06-27 17:56:07 +1000

    Update WAV2TXT.h

--- WAV2TXT.h
+++ WAV2TXT.h
@@ -7,24 +7,21 @@
 #include <cmath>
 
 struct WAVHeader {
-    // RIFF заголовок
     char chunkID[4];        // "RIFF"
-    uint32_t chunkSize;     // Размер файла - 8
+    uint32_t chunkSize;     // 8
     char format[4];         // "WAVE"
 
-    // fmt подзаголовок
     char subchunk1ID[4];    // "fmt "
-    uint32_t subchunk1Size; // 16 для PCM
-    uint16_t audioFormat;   // 1 для PCM
-    uint16_t numChannels;   // 1 - моно, 2 - стерео
-    uint32_t sampleRate;    // Частота дискретизации
+    uint32_t subchunk1Size; // 16 PCM
+    uint16_t audioFormat;   // 1 PCM
+    uint16_t numChannels;
+    uint32_t sampleRate;
     uint32_t byteRate;      // sampleRate * numChannels * bitsPerSample/8
     uint16_t blockAlign;    // numChannels * bitsPerSample/8
     uint16_t bitsPerSample; // 8, 16, 24, 32
 
-    // data подзаголовок
     char subchunk2ID[4];    // "data"
-    uint32_t subchunk2Size; // Размер данных
+    uint32_t subchunk2Size;
 };
 
 class WAVConverter {
